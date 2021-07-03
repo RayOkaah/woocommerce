3bb9831c5dc2202ccab823e8c798793c146eb6bf
Mubeen Ur Rehman <mrjazib1@gmail.com>
2021-07-03 15:44:49 +0500

    cart coupon and shipping rate model added

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -34,6 +34,7 @@
 import 'package:woocommerce/models/customer.dart';
 
 import 'cart_item.dart';
+import 'shipping_rates.dart';
 
 class WooCart {
   WooCart({
@@ -49,8 +50,8 @@ class WooCart {
     this.errors,
   });
 
-  List<dynamic>? coupons;
-  List<dynamic>? shippingRates;
+  List<WooCartCoupon>? coupons;
+  List<ShippingRates>? shippingRates;
   Shipping? shippingAddress;
   List<WooCartItem>? items;
   int? itemsCount;
@@ -61,8 +62,8 @@ class WooCart {
   List<dynamic>? errors;
 
   factory WooCart.fromJson(Map<String, dynamic> json) => WooCart(
-        coupons: List<dynamic>.from(json["coupons"].map((x) => x)),
-        shippingRates: List<dynamic>.from(json["shipping_rates"].map((x) => x)),
+        coupons: List<WooCartCoupon>.from(json["coupons"].map((x) => WooCartCoupon.fromJson(x))),
+        shippingRates: List<ShippingRates>.from(json["shipping_rates"].map((x) => ShippingRates.fromJson(x))),
         shippingAddress: Shipping.fromJson(json["shipping_address"]),
         items: List<WooCartItem>.from(json["items"].map((x) => WooCartItem.fromJson(x))),
         itemsCount: json["items_count"],
@@ -74,8 +75,8 @@ class WooCart {
       );
 
   Map<String, dynamic> toJson() => {
-        "coupons": List<dynamic>.from(coupons!.map((x) => x)),
-        "shipping_rates": shippingRates!.map((x) => x).toList(),
+        "coupons": coupons!.map((x) => x.toJson()).toList(),
+        "shipping_rates": shippingRates!.map((x) => x.toJson()).toList(),
         "shipping_address": shippingAddress!.toJson(),
         "items": items!.map((x) => x.toJson()).toList(),
         "items_count": itemsCount,
@@ -88,4 +89,76 @@ class WooCart {
 
   @override
   toString() => this.toJson().toString();
+}
+
+class WooCartCoupon {
+  String? code;
+  String? discountType;
+  WooCartCouponTotals? totals;
+
+  WooCartCoupon({
+    this.code,
+    this.discountType,
+    this.totals,
+  });
+
+  factory WooCartCoupon.fromJson(Map<String, dynamic> json) => WooCartCoupon(
+        code: json['code'],
+        discountType: json['discount_type'],
+        totals: WooCartCouponTotals.fromJson(json['totals']),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'code': code,
+        'discount_type': discountType,
+        'totals': totals!.toJson(),
+      };
+}
+
+class WooCartCouponTotals {
+  String? totalDiscount;
+  String? totalDiscountTax;
+  String? currencyCode;
+  String? currencySymbol;
+  int? currencyMinorUnit;
+  String? currencyDecimalSeparator;
+  String? currencyThousandSeparator;
+  String? currencyPrefix;
+  String? currencySuffix;
+
+  WooCartCouponTotals({
+    this.totalDiscount,
+    this.totalDiscountTax,
+    this.currencyCode,
+    this.currencySymbol,
+    this.currencyMinorUnit,
+    this.currencyDecimalSeparator,
+    this.currencyThousandSeparator,
+    this.currencyPrefix,
+    this.currencySuffix,
+  });
+
+  factory WooCartCouponTotals.fromJson(Map<String, dynamic> json) => WooCartCouponTotals(
+        totalDiscount: json['total_discount'],
+        totalDiscountTax: json['total_discount_tax'],
+        currencyCode: json['currency_code'],
+        currencySymbol: json['currency_symbol'],
+        currencyMinorUnit: json['currency_minor_unit'],
+        currencyDecimalSeparator: json['currency_decimal_separator'],
+        currencyThousandSeparator: json['currency_thousand_separator'],
+        currencyPrefix: json['currency_prefix'],
+        currencySuffix: json['currency_suffix'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'total_discount': totalDiscount,
+        'total_discount_tax': totalDiscountTax,
+        'currency_code': currencyCode,
+        'currency_symbol': currencySymbol,
+        'currency_minor_unit': currencyMinorUnit,
+        'currency_decimal_separator': currencyDecimalSeparator,
+        'currency_thousand_separator': currencyThousandSeparator,
+        'currency_prefix': currencyPrefix,
+        'currency_suffix': currencySuffix,
+      };
 }
